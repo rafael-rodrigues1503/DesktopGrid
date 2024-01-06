8bed0e3a4e38cd545389591f64024d82842262b0
Rafael Rodrigues <rafael.rodrigues.rr1503@gmail.com>
2024-01-05 23:11:48 -0300

    Refactored grid lines and markers

---
+++ colors.py
@@ -0,0 +1,13 @@
+color_palette = {
+    'grid-background':                      '#FAEBD7', # Antique White
+
+    'gridLines-axes':                       '#000000', # Black
+    'gridLines-hundreds':                   '#D9D9D9', # Gray90
+    'gridLines-twenties':                   '#E5E5E5', # Gray85
+
+    'toolsMenu-background':                 '#F5F6F7',
+    'toolsMenu-tool-mouseover-background':  '#E8EFF7',
+    'toolsMenu-tool-mouseover-border':      '#A4CEF9',
+    'toolsMenu-tool-selected-background':   '#C9E0F7',
+    'toolsMenu-tool-selected-border':       '#62A2E4',
+}
--- desktop_grid.py
+++ desktop_grid.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tools import *
+from colors import color_palette
 
 class DesktopGrid:
 
@@ -8,6 +9,7 @@ class DesktopGrid:
         self.root = Tk()
         self.root.title("Desktop Grid")
         self.root.attributes("-fullscreen", True)
+        self.root.resizable(0,0)
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
         self.root.bind('<KeyPress>', lambda e: self.root.destroy() if e.char.lower() == 'q' else None)
@@ -21,43 +23,141 @@ class DesktopGrid:
 
     def _create_grid(self):
 
-        self.canvas = Canvas(self.root, bg='antique white', highlightthickness=0)
-        self.canvas.grid(sticky='nsew')
+        self.canvas = Canvas(
+            self.root,
+            bg=color_palette['grid-background'],
+            highlightthickness=0
+            )
+        self.canvas.grid(sticky=NSEW)
 
+        # Create the 20s grid lines
         for n in range(20, self.width // 2, 20):
 
-            if n % 100 == 0:
-                _fill = 'gray85'
-                _width = 2
-            else:
-                _fill = 'gray90'
-                _width = 1
-
-            self.canvas.create_line(self.width // 2 + n, 0, self.width // 2 + n, self.height, fill=_fill, width=_width)
-            self.canvas.create_line(self.width // 2 - n, 0, self.width // 2 - n, self.height, fill=_fill, width=_width)
-
-            self.canvas.create_line(0, self.height // 2 + n, self.width, self.height // 2 + n, fill=_fill, width=_width)
-            self.canvas.create_line(0, self.height // 2 - n, self.width, self.height // 2 - n, fill=_fill, width=_width)
+            self.canvas.create_line(
+                self.width // 2 + n, 0,
+                self.width // 2 + n, self.height,
+                fill=color_palette['gridLines-twenties'],
+                width=1
+                )
+            self.canvas.create_line(
+                self.width // 2 - n, 0,
+                self.width // 2 - n, self.height,
+                fill=color_palette['gridLines-twenties'],
+                width=1
+                )
+            self.canvas.create_line(
+                0, self.height // 2 + n,
+                self.width, self.height // 2 + n,
+                fill=color_palette['gridLines-twenties'],
+                width=1
+                )
+            self.canvas.create_line(
+                0, self.height // 2 - n,
+                self.width, self.height // 2 - n,
+                fill=color_palette['gridLines-twenties'],
+                width=1
+                )
         
-            if n % 100 == 0:
+        # Create the 100s grid lines
+        for n in range(100, self.width // 2, 100):
 
-                self.canvas.create_rectangle(self.width // 2 + n - 11, self.height // 2 + 12, self.width // 2 + n + 11, self.height // 2 + 27, fill='antique white', outline='')
-                self.canvas.create_rectangle(self.width // 2 - n - 11, self.height // 2 + 12, self.width // 2 - n + 11, self.height // 2 + 27, fill='antique white', outline='')
-                self.canvas.create_rectangle(self.width // 2 - 21, self.height // 2 + n - 7, self.width // 2 - 9, self.height // 2 + n + 7, fill='antique white', outline='')
-                self.canvas.create_rectangle(self.width // 2 - 21, self.height // 2 - n - 7, self.width // 2 - 9, self.height // 2 - n + 7, fill='antique white', outline='')
+            self.canvas.create_line(
+                self.width // 2 + n, 0,
+                self.width // 2 + n, self.height,
+                fill=color_palette['gridLines-hundreds'],
+                width=3
+                )
+            self.canvas.create_line(
+                self.width // 2 - n, 0,
+                self.width // 2 - n, self.height,
+                fill=color_palette['gridLines-hundreds'],
+                width=3
+                )
+            self.canvas.create_line(
+                0, self.height // 2 + n,
+                self.width, self.height // 2 + n,
+                fill=color_palette['gridLines-hundreds'],
+                width=3
+                )
+            self.canvas.create_line(
+                0, self.height // 2 - n,
+                self.width, self.height // 2 - n,
+                fill=color_palette['gridLines-hundreds'],
+                width=3
+                )
+            
+        # Create the axis lines
+        self.canvas.create_line(
+            0, self.height // 2,
+            self.width, self.height // 2,
+            fill=color_palette['gridLines-axes'],
+            width=5
+            )
+        self.canvas.create_line(
+            self.width // 2, 0,
+            self.width // 2, self.height,
+            fill=color_palette['gridLines-axes'],
+            width=5
+            )
+        
+        # Create the markers
+        for n in range(100, self.width, 100):
+
+                self.canvas.create_rectangle(
+                    self.width // 2 + n - 12, self.height // 2 + 12,
+                    self.width // 2 + n + 13, self.height // 2 + 29,
+                    fill=color_palette['grid-background'],
+                    outline=''
+                    )
+                self.canvas.create_rectangle(
+                    self.width // 2 - n - 12, self.height // 2 + 12,
+                    self.width // 2 - n + 13, self.height // 2 + 29,
+                    fill=color_palette['grid-background'],
+                    outline=''
+                    )
+                self.canvas.create_rectangle(
+                    self.width // 2 - 32, self.height // 2 + n - 8,
+                    self.width // 2 - 7, self.height // 2 + n + 9,
+                    fill=color_palette['grid-background'],
+                    outline=''
+                    )
+                self.canvas.create_rectangle(
+                    self.width // 2 - 32, self.height // 2 - n - 8,
+                    self.width // 2 - 7, self.height // 2 - n + 9,
+                    fill=color_palette['grid-background'],
+                    outline=''
+                    )
                 
-                self.canvas.create_text(self.width // 2 + n, self.height // 2 + 20, text=str(n), anchor='center')
-                self.canvas.create_text(self.width // 2 - n, self.height // 2 + 20, text=str(n), anchor='center')
-                self.canvas.create_text(self.width // 2 - 20, self.height // 2 + n, text=str(n), anchor='center')
-                self.canvas.create_text(self.width // 2 - 20, self.height // 2 - n, text=str(n), anchor='center')
-
-        self.canvas.create_line(0, self.height // 2, self.width, self.height // 2, fill='black', width=5)
-        self.canvas.create_line(self.width // 2, 0, self.width // 2, self.height, fill='black', width=5)
+                self.canvas.create_text(
+                    self.width // 2 + n, self.height // 2 + 20,
+                    text=str(n),
+                    anchor=CENTER
+                    )
+                self.canvas.create_text(
+                    self.width // 2 - n, self.height // 2 + 20,
+                    text=str(n),
+                    anchor=CENTER
+                    )
+                self.canvas.create_text(
+                    self.width // 2 - 20, self.height // 2 + n,
+                    text=str(n),
+                    anchor=CENTER
+                    )
+                self.canvas.create_text(
+                    self.width // 2 - 20, self.height // 2 - n,
+                    text=str(n),
+                    anchor=CENTER
+                    )
 
 
     def _create_tools_menu(self):
 
-        self.tools_menu = Frame(self.canvas, borderwidth=2, relief='groove', background='#F5F6F7')
+        self.tools_menu = Frame(
+            self.canvas,
+            borderwidth=2,
+            relief='groove',
+            bg=color_palette["toolsMenu-background"]
+            )
         self.tools_menu.grid(padx=5, pady=5)
 
         self.icons = [
@@ -68,7 +168,14 @@ class DesktopGrid:
             PhotoImage(file='icons\Line.png').subsample(2, 2),
             PhotoImage(file='icons\Text.png').subsample(2, 2)
         ]
-        self.tools = [Tool(lambda _: 1, self.tools_menu, image=i, background='#F5F6F7', activebackground='#C9E0F7', borderwidth=0) for i in self.icons]
+        self.tools = [Tool(
+            lambda _: 1,
+            self.tools_menu,
+            image=i,
+            bg=color_palette["toolsMenu-background"],
+            activebackground=color_palette["toolsMenu-tool-selected-background"],
+            borderwidth=0
+            ) for i in self.icons]
 
         self.tool_activated = False
         self.selected_tool = False
@@ -87,27 +194,27 @@ class DesktopGrid:
 
             case EventType.Enter:
                 if event.widget != self.selected_tool:
-                    event.widget.config(background="#E8EFF7")
+                    event.widget.config(bg=color_palette["toolsMenu-tool-mouseover-background"])
 
             case EventType.Leave:
                 if event.widget != self.selected_tool:
-                    event.widget.config(background='#F5F6F7')
+                    event.widget.config(bg=color_palette["toolsMenu-background"])
 
             case EventType.ButtonRelease:
 
                 if self.tool_activated:
-                    self.selected_tool.config(background='#F5F6F7')
+                    self.selected_tool.config(bg=color_palette["toolsMenu-background"])
 
                     if self.selected_tool == event.widget:
                         self.tool_activated = False
                         self.selected_tool = False
 
                     else:
-                        event.widget.config(background="#C9E0F7")
+                        event.widget.config(bg=color_palette["toolsMenu-tool-selected-background"])
                         self.selected_tool = event.widget
 
                 else:
-                    event.widget.config(background="#C9E0F7")
+                    event.widget.config(bg=color_palette["toolsMenu-tool-selected-background"])
             
                     self.tool_activated = True
                     self.selected_tool = event.widget
